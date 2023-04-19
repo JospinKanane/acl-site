b88daef41e72b39f97798d8d70da865e3737175b
JospinKanane <jospinkanane@yahoo.com>
2023-04-19 15:27:41 +0200

    Adding navbar in our website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.png';
+import React from 'react';
 import './App.css';
+import Menu from './components/menu/Menu.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is the <code>official website</code> of Art Congo Lab designed by Art-Code.
-        </p>
-        <a
-          className="App-link"
-          href="https://dev-acl-news.pantheonsite.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visit the Art Congo Lab blog
-        </a>
+      <header className="App-header w-full shadow-none">
+        <Menu/>
       </header>
     </div>
   );
